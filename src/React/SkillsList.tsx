07362d102c786913129405add45568c97ba0e7dd
marwanchkiriba <marwanchkiriba9@gmail.com>
2025-06-17 21:15:52 +0200

    first commit

---
+++ src/React/SkillsList.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SkillsList = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+    </div>
+  );
+};
+
+export default SkillsList;
